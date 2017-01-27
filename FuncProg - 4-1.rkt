ab594b18b960d64cf9e6ee618e001cbc5389f4ce
Brittany Miller <brittany.miller@bison.howard.edu>
2017-01-27 04:28:49 -0500

    number 4
    Helper function made to make list excluding non numbers. Also attempting
    to find minimum values in both lists

--- FuncProg - 4-1.rkt
+++ FuncProg - 4-1.rkt
@@ -8,8 +8,8 @@
 (DEFINE (min-above-min L1 L2)
         (COND
          ((NULL? L1) #F)
-         ((AND (NULL? L2) (NOT (NULL? L1))) (MIN (new-list L1)))
-         
+         ((AND (NULL? L2) (NOT (NULL? L1))) (< (CAR(new-list L1)) (CDR (new-list L1))))
+         ((NOT (NULL? (CAR L2)))(> (< (CAR(new-list L1)) (CDR (new-list L1))) (< (CAR(new-list L2)) (CDR (new-list L2)))))
 
         )
 )
