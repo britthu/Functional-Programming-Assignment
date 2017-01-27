b7771a0538f2385e0fe37d6e573c753daf210391
Brittany Miller <brittany.miller@bison.howard.edu>
2017-01-26 19:57:43 -0500

    number 3
    reads list up until first sublist but does not reast of list

---
+++ FuncProg - 3.rkt
@@ -0,0 +1,9 @@
+(DEFINE (sum-up-numbers-general L)
+	(COND
+		((NOT(LIST? L)) 0)
+		((NULL? L) 0)
+		((number? (CAR L))(+ (CAR L) (sum-up-numbers-general(CDR L))))
+                ((LIST? (CAR L)) (sum-up-numbers-general (CAR L)))
+                (ELSE (sum-up-numbers-general(CDR L)))
+         )
+)
